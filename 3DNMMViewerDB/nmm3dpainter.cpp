4d9c8641bb7e45a24aa05fcb9885c8c2374506e3
Kacper Pluta <kacper.pluta@esiee.fr>
2017-04-28 15:19:51 +0200

    Color maps

--- 3DNMMViewerDB/nmm3dpainter.cpp
+++ 3DNMMViewerDB/nmm3dpainter.cpp
@@ -18,14 +18,36 @@ NMM3DPainter::NMM3DPainter(QWidget *parent) :
     QPalette pal( palette() );
     pal.setColor( QPalette::Background, Qt::white );
 
-    colorMap["[1, 0, 0]"] = QPalette(Qt::green);
-    colorMap["[0, 1, 0]"] = QPalette(QColor (128, 0, 128));
-    colorMap["[0, 0, 1]"] = QPalette(Qt::magenta);
-    colorMap["[-1, 0, 0]"] = QPalette(QColor (255, 165, 0));
-    colorMap["[0, -1, 0]"] = QPalette(Qt::yellow);
-    colorMap["[0, 0, -1]"] = QPalette(Qt::cyan);
+    /*N1*/
+    colorMap["[1, 0, 0]"] = QPalette(QColor (213, 255, 0));
+    colorMap["[0, 1, 0]"] = QPalette(QColor (255, 0, 86));
+    colorMap["[0, 0, 1]"] = QPalette(QColor (158, 0, 142));
+    colorMap["[-1, 0, 0]"] = QPalette(QColor (14, 76, 161));
+    colorMap["[0, -1, 0]"] = QPalette(QColor (0, 95, 57));
+    colorMap["[0, 0, -1]"] = QPalette(QColor (0, 255, 0));
     colorMap["[0, 0, 0]"] = QPalette(Qt::black);
-
+    /*N2*/
+    colorMap["[1, 1, 0]"] = QPalette(QColor (149, 0, 58));
+    colorMap["[1, 0, 1]"] = QPalette(QColor (255, 147, 126));
+    colorMap["[0, 1, 1]"] = QPalette(QColor (164, 36, 0));
+    colorMap["[-1, -1, 0]"] = QPalette(QColor (67, 0, 44));
+    colorMap["[-1, 0, -1]"] = QPalette(QColor (145, 208, 203));
+    colorMap["[0, -1, -1]"] = QPalette(QColor (107, 104, 130));
+    colorMap["[-1, 1, 0]"] = QPalette(QColor (0, 0, 255));
+    colorMap["[-1, 0, 1]"] = QPalette(QColor (0, 125, 181));
+    colorMap["[1, -1, 0]"] = QPalette(QColor (106, 130, 108));
+    colorMap["[1, 0, -1]"] = QPalette(QColor (0, 174, 126));
+    colorMap["[0, -1, 1]"] = QPalette(QColor (194, 140, 159));
+    colorMap["[0, 1, -1]"] = QPalette(QColor (190, 153, 112));
+    /*N3*/
+    colorMap["[1, 1, 1]"] = QPalette(QColor (0, 143, 156));
+    colorMap["[-1, 1, 1]"] = QPalette(QColor (95, 173, 78));
+    colorMap["[1, -1, 1]"] = QPalette(QColor (255, 0, 0));
+    colorMap["[1, 1, -1]"] = QPalette(QColor (255, 0, 246));
+    colorMap["[-1, -1, 1]"] = QPalette(QColor (255, 2, 157));
+    colorMap["[-1, 1, -1]"] = QPalette(QColor (104, 61, 59));
+    colorMap["[-1, -1, -1]"] = QPalette(QColor (255, 116, 163));
+    colorMap["[1, -1, -1]"] = QPalette(QColor (150, 138, 232));
 
     // bottom -2
     pixels.insert ( std::make_pair ( QString ( "[-1, -2, -1]" ), ui->m1m2m1 ) );
@@ -49,7 +71,6 @@ NMM3DPainter::NMM3DPainter(QWidget *parent) :
     pixels.insert ( std::make_pair ( QString ( "[-2, -2, -1]" ), ui->m2m2m1) );
     pixels.insert ( std::make_pair ( QString ( "[-2, -2, 0]" ), ui->m2m2pm0) );
     pixels.insert ( std::make_pair ( QString ( "[-2, -2, 1]" ), ui->m2m2p1) );
-
 
     // bottom -1
     pixels.insert ( std::make_pair ( QString ( "[-1, -1, -1]" ), ui->m1m1m1 ) );
@@ -154,7 +175,6 @@ NMM3DPainter::NMM3DPainter(QWidget *parent) :
     pixels.insert ( std::make_pair ( QString ( "[-2, 2, -1]" ), ui->m2p2m1) );
     pixels.insert ( std::make_pair ( QString ( "[-2, 2, 0]" ), ui->m2p2pm0) );
     pixels.insert ( std::make_pair ( QString ( "[-2, 2, 1]" ), ui->m2p2p1) );
-
 
     for ( auto &it:pixels )
         it.second->clear();
